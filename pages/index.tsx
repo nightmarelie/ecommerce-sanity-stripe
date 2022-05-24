24d3f709ce1dacebbdd6f44c55e59ed136a19949
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-23 20:37:29 -0600

    feat: add styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,14 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Best selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["product 1", "product 2"].map((p) => p)}</div>
+      <div className="products-container">
+        {["product 1", "product 2"].map((p) => p)}
+      </div>
+      Footer
     </>
   );
 };
