e53824178feeb4ca150939e0f14571c286994b2b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-23 20:29:19 -0600

    feat: print products

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home = () => {
         <h2>Best selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
+      <div>{["product 1", "product 2"].map((p) => p)}</div>
     </>
   );
 };
