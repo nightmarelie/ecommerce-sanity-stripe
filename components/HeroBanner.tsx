1e28cb3eecd625ae2c590da19d78b7b4e513ccc8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-23 21:12:23 -0600

    feat: add hero banner

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 export const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="hero-banner-container">
+      <div className="beats-solo">HeroBanner</div>
+      <h3>Mid text</h3>
+      <img src="" alt="Headphones" className="hero-banner-image" />
+      <div>
+        <Link href="/product/ID">
+          <button>Button Text</button>
+        </Link>
+      </div>
+      <div className="desc">
+        <h5>Description</h5>
+        <p>Description</p>
+      </div>
+    </div>
+  );
 };
