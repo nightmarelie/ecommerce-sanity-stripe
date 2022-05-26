dd7f312fa4833102c9a9efaf67937b8b8090c27f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-26 12:32:18 -0600

    feat: modify error helper

--- lib/error-helper.ts
+++ lib/error-helper.ts
@@ -1,7 +1,30 @@
-const getErrorMsg = (error: unknown) => {
-  if (error instanceof Error) return error.message;
+import { stringify } from "querystring";
 
-  return String(error);
+type ErrorWithMessage = {
+  message: string;
+};
+
+const isErrorWithMessage = (error: unknown): error is ErrorWithMessage => {
+  return (
+    typeof error == "object" &&
+    error !== null &&
+    "message" in error &&
+    typeof (error as Record<string, unknown>).message == "string"
+  );
+};
+
+const toErrorWithMessage = (error: unknown): ErrorWithMessage => {
+  if (isErrorWithMessage(error)) return error;
+
+  try {
+    return new Error(JSON.stringify(error));
+  } catch {
+    return new Error(String(error));
+  }
+};
+
+const getErrorMsg = (error: unknown) => {
+  return toErrorWithMessage(error);
 };
 
 export { getErrorMsg };
